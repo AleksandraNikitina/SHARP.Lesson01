a02c2fe095ee70e20bc0bff5e09292c3f26b06e9
Aleksandra <asandrabelka@gmail,com>
2024-10-14 23:58:34 +0300

    Added Folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Write a program that takes natural number N  as input 
+// and shows whether the first number is the square of the second.
+
+// a = 25, b = 5 =>> yes
+// a = 2, b = 18 =>> no
+// a = 9, b = -3 =>> yes
+// a = -3, b = 9 =>> no
+
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// or 
+// string num1str = Console.ReadLine();
+// int num1 = Convert.ToInt32(num1str); 
+           
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("First number is square of second number.");
+}
+else
+{
+    Console.WriteLine("First number is not square of second number.");
+}
