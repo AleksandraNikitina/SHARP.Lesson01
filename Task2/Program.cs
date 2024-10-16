f8607862251f268ff569792c7a79664d6569a2ec
Aleksandra <asandrabelka@gmail,com>
2024-10-16 18:07:48 +0300

    Added check

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,13 +4,22 @@
 // 4 =>> -4, -3, -2, -1, 0, 1, 2, 3, 4
 // 2 =>> -2, -1, 0, 1, 2
 
-Console.WriteLine("Input positive integer: ");
+Console.WriteLine("Enter positive integer: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num > 0)
+{
 int count = -num;
 while (count <= num)
 {
   Console.Write(count + ", "); 
   count++; //count = count+1;
 }
+}
+
+else 
+{
+  Console.WriteLine("Enter error");
+}
+
 
