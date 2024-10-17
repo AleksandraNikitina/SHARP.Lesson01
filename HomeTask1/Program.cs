faf39f82959953e60bf2a010eaaa502ebd1164e2
Aleksandra <asandrabelka@gmail,com>
2024-10-18 02:20:32 +0300

    Added some system files

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,9 +1,9 @@
-﻿// Write a program that takes two integer as input and 
+﻿// Write a program that takes two integers as input and 
 // shows what is the biggest and what is the smallest.  
 // a=5, b=7 => max=7, min=5
 // a=-9, b=-3 => max=-3, min=-9
 
-Console.WriteLine("Enter two integer: ");
+Console.WriteLine("Enter two integers: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 
