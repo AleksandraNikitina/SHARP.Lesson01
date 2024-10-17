379b6d07ee5aa48b4e98ae17a21be5c57d2ea18b
Aleksandra <asandrabelka@gmail,com>
2024-10-18 01:13:28 +0300

    Added ptogramTask3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a program that takes three-digit integer as input and 
+// shows on screen sum of the first and the last digits.  
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+Console.WriteLine("Enter positive three-digit integer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstDigit = num / 100; // 456 / 100 = 4,56
+    int lastDigit = num % 10; // 456 % 10 = 450 + 6
+    int sum = firstDigit + lastDigit;
+    Console.WriteLine($"Sum of the first and the last digits = {sum}");
+}
+else
+{
+    Console.WriteLine("Enter error! Number is not a three-digit.");
+}
