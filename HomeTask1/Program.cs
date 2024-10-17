d379927ac9957238afd25c7c1979fa6a7cf6f3bc
Aleksandra <asandrabelka@gmail,com>
2024-10-18 01:43:42 +0300

    Added program HomeTask1

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Write a program that takes two integer as input and 
+// shows what is the biggest and what is the smallest.  
+// a=5, b=7 => max=7, min=5
+// a=-9, b=-3 => max=-3, min=-9
+
+Console.WriteLine("Enter two integer: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}, min = {num2}");
+}
+else 
+{
+    if (num1 == num2)
+    {
+        Console.WriteLine($"Numbers are equal. max = min = {num1}");    
+    }
+    else
+    {
+        Console.WriteLine($"max = {num2}, min = {num1}");
+    }
+}
