65bcb2834e595b29f4c5149d489fb648c14174c4
Aleksandra <asandrabelka@gmail,com>
2024-10-18 03:19:21 +0300

    Added Correct Program Task1

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -3,22 +3,41 @@
 // a=5, b=7 => max=7, min=5
 // a=-9, b=-3 => max=-3, min=-9
 
-Console.WriteLine("Enter two integers: ");
+
+// Console.WriteLine("Enter two integers: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"max = {num1}, min = {num2}");
+// }
+// else 
+// {
+//     if (num1 == num2)
+//     {
+//         Console.WriteLine($"Numbers are equal. max = min = {num1}");    
+//     }
+//     else
+//     {
+//         Console.WriteLine($"max = {num2}, min = {num1}");
+//     }
+// }
+
+// Write a program that takes two integers as input and 
+// checks is the first number divisible by the second number.  
+
+Console.WriteLine("Enter two integers.");
+Console.Write("First number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{
-    Console.WriteLine($"max = {num1}, min = {num2}");
-}
+ if ((num1 % num2) == 0)
+ {
+    Console.WriteLine("Divisible");
+ }
 else 
 {
-    if (num1 == num2)
-    {
-        Console.WriteLine($"Numbers are equal. max = min = {num1}");    
-    }
-    else
-    {
-        Console.WriteLine($"max = {num2}, min = {num1}");
-    }
+    Console.WriteLine("Not divisible");
 }
