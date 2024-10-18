93aa2331df6db758998674a72d0b0a50ba620301
Aleksandra <asandrabelka@gmail,com>
2024-10-18 03:38:49 +0300

    Added program Task3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write a program that takes number as input and 
+// checks is this number positive, negative or zero.  
+
+
+Console.Write("Enmter the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0) 
+{
+    Console.WriteLine("Number is positive.");
+}
+else if (num < 0)
+{
+    Console.WriteLine("Number is negative.");
+}
+else 
+{
+    Console.WriteLine("Number is zero.");
+}
