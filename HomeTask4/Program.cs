82cb773ff1044c311e3a7a1f00cc1113f33f2e61
Aleksandra <asandrabelka@gmail,com>
2024-10-18 10:50:28 +0300

    Added some system file and progran Hometask4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Write a program that takes a number as input 
+// and shows all odd numbers  starting from 1 to this number (including).
+
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+if (num < 1)
+{
+    Console.WriteLine("Number is lowest then one.");
+}
+while (num >= current)
+{
+    Console.Write($"{current} ");
+    current = current +2;
+}
