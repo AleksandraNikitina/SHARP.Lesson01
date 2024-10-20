3fca5d859ce62561c8d06290dfd67a85ea32cf20
Aleksandra <asandrabelka@gmail,com>
2024-10-20 16:51:36 +0300

    Added Lecture1Task5

---
+++ Lecture1Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Enter N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+while (current <= n) 
+{
+    // Console.Write($"{current} ");
+    Console.Write(current);
+    Console.Write(' ');
+    current ++; //current = current +1
+}
