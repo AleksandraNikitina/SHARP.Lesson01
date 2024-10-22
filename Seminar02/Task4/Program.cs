30858f4fdc82e4f9ae5454a78eb87a47974cfe4c
Aleksandra <asandrabelka@gmail,com>
2024-10-22 17:49:31 +0300

    Add Task4

---
+++ Seminar02/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write a program that outputs the third (from left) digit of the input number
+// or reports that there is no 3rd digit.
+
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 || num <= -100)
+{
+    while (num > 999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int thirdDigit = num % 10;
+    Console.WriteLine(thirdDigit);
+}
+else
+{
+    Console.WriteLine("There is no the third digit.");
+}
