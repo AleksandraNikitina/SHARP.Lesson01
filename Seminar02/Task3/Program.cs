290766cb90a460b1d41136ece6c4cffc679a58ed
Aleksandra <asandrabelka@gmail,com>
2024-10-22 15:52:58 +0300

    Program Task3

---
+++ Seminar02/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter two integers.");
+Console.Write("First number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 == 0)
+{
+    Console.WriteLine("You can't divide by zero.");
+}
+else if (num1 % num2 == 0)
+{
+    Console.WriteLine("Divisible");
+}
+else
+{
+    int remainder = num1 % num2;
+    Console.WriteLine($"Remainder from division = {remainder}");
+}
