03450452c35ca283d2f18582419f62b3bb08d626
Aleksandra <asandrabelka@gmail,com>
2024-10-22 18:48:42 +0300

    Add HomeTask3

---
+++ Seminar02/HomeTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Enter integer number of range [10..99]: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine($"Maximal digit: {firstDigit}");
+    }
+    else
+    {
+        Console.WriteLine($"Maximal digit: {secondDigit}");
+    }
+}
+else
+{
+    Console.WriteLine("Enter error");
+}
