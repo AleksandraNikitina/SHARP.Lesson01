01d3fcd6e7b16ffb2774f11f8c866ec3a5378818
Aleksandra <asandrabelka@gmail,com>
2024-10-22 18:35:59 +0300

    Add HomeTask 1&2

---
+++ Seminar02/HomeTask1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%7 == 0 && num%23 ==0)
+{
+    Console.WriteLine("Divisible");
+}
+else
+{
+    Console.WriteLine("Not divisible");
+}
---
+++ Seminar02/HomeTask2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0)
+{
+    if (y>0)
+    {
+        Console.WriteLine("First coordinate quadrant");
+    }
+    else
+    {
+        Console.WriteLine("Fourth coordinate quadrant");
+    }
+}
+else if (y > 0)
+{
+    Console.WriteLine("Second coordinate quadrant");
+}
+else
+{
+    Console.WriteLine("Third coordinate quadrant");
+}
