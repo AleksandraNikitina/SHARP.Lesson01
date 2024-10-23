098a54f261510fec972c89e7bf6fed7b421cdf78
Aleksandra <asandrabelka@gmail,com>
2024-10-23 16:02:54 +0300

    Add example progtam from seminar

---
+++ Seminar02/HomeTask4/Example/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10)
+{
+    Console.WriteLine(num);
+}
+else
+{
+    while (num > 0)
+    {
+        int currentDigit = num % 10;
+        num /= 10;
+        if (num > 0)
+        {
+        Console.Write(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
