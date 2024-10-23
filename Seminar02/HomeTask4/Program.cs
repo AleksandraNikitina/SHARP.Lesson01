e23a0fafa0c2f92a30de26c56441e651d3271807
Aleksandra <asandrabelka@gmail,com>
2024-10-23 15:51:18 +0300

    Add HomeTask4

---
+++ Seminar02/HomeTask4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int firstRank = 1;
+
+for (int currentRank = 1; num/currentRank != 0; currentRank = currentRank * 10)
+{
+    firstRank = currentRank;
+}
+// Console.WriteLine(firstRank);
+
+for (int i = firstRank; i > 0; i = i / 10)
+{
+    int digit = num / i;
+    num = num - digit * i;
+    Console.Write(digit);
+    if (i != 1)
+    {
+        Console.Write(", ");
+    }
+}
